af6c6ff67105765b3b778c59bc4cd9ca930ad0fd
vagrant <vagrant@vagrant.wharton.upenn.edu>
2015-10-29 16:07:09 +0000

    Moving results call within condition block to prevent an indexerror if results are empty

--- wharton_cosign_auth/remote_user.py
+++ wharton_cosign_auth/remote_user.py
@@ -9,8 +9,8 @@ class WhartonRemoteUserBackend(RemoteUserBackend):
     def configure_user(self, user):
         response = call_wisp_api(
             'https://apps.wharton.upenn.edu/wisp/api/v1/adusers', {'username': user.username})
-        results = response['results'][0]
-        if results:
+        if response:
+            results = response['results'][0]
             user.first_name = results['first_name']
             user.last_name = results['last_name']
             user.email = results['email'].replace('exchange.', '')
