7e68cd1b40f9fb85f3da3b7053398ecda1c804f0
Roller, David <roller@wharton.upenn.edu>
2016-09-22 13:22:01 -0400

    Added more descriptive Exception when WISP returns non JSON data

--- wharton_cosign_auth/utilities.py
+++ wharton_cosign_auth/utilities.py
@@ -5,5 +5,8 @@ import requests
 
 def call_wisp_api(url=None, params=None):
     headers = {'Authorization': 'Token %s' % settings.WISP_TOKEN}
-    response = requests.get(url, headers=headers, params=params).json()
+    try:
+        response = requests.get(url, headers=headers, params=params).json()
+    except ValueError as err:
+        raise Exception('WISP did not return valid JSON. This may be due to WISP API being down.') from err
     return response
